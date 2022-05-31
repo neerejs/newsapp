bffe0f46e1d32f2a11c6f1e714c0c8e62aa744cc
Neerej Selvakumar <neerejselvakumar@Neerejs-MacBook-Pro.local>
2022-05-31 12:31:34 -0400

    more changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+
 import News from './News';
 
 function App() {
--- src/News.js
+++ src/News.js
@@ -5,12 +5,15 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Image from 'react-bootstrap/Image';
 import { Container } from 'react-bootstrap';
 import axios from 'axios';
-
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
 
 
 const News = () => {
 
     const [news, setNews] = useState([]);
+    const [searchtext, setSearchText] = useState([])
 
 
 
@@ -22,22 +25,42 @@ const News = () => {
         // eslint-disable-next-line
     }, []);
 
-    const loadData = async () => {
+    const loadData = async (searchvalue) => {
         // const response = await fetch(API_URL);
         // const data = await response.json();
         // setNews(data.articles);
         // console.log(news);
+        var options;
 
-        
+        if (searchvalue) {
 
-        var options = {
-            method: 'GET',
-            url: 'https://api.newscatcherapi.com/v2/search',
-            params: { q: 'Bitcoin', lang: 'en', sort_by: 'relevancy', page: '1' },
-            headers: {
-                'x-api-key': API_KEY
-            }
-        };
+            options = {
+                method: 'GET',
+                url: 'https://api.newscatcherapi.com/v2/search',
+                params: {
+                    q: searchvalue,
+                    lang: 'en',
+                    sort_by: 'relevancy',
+                    page: '1'
+                },
+                headers: {
+                    'x-api-key': API_KEY
+                }
+            };
+        }
+
+        else {
+
+
+            options = {
+                method: 'GET',
+                url: 'https://api.newscatcherapi.com/v2/search',
+                params: { q: 'Bitcoin', lang: 'en', sort_by: 'relevancy', page: '1' },
+                headers: {
+                    'x-api-key': API_KEY
+                }
+            };
+        }
 
         axios.request(options).then(function (response) {
             console.log(response.data);
@@ -60,24 +83,34 @@ const News = () => {
 
 
                     <ListGroup.Item>
-                        
-                            <Row>
 
-                                <Col md={3}>
-                                    <Image fluid src={item.media} />
+                        <Row>
 
-                                </Col>
-                                <Col md={5}>
-                                    {item.summary}
-                                    <br></br>
-                                    <a rel="noreferrer" target = "_blank" href = {item.link}>Read More</a>
-                                </Col>
-                                <Col md={4}>
-                                    
-                                </Col>
+                         
+                            <Col >
 
-                            </Row>
-                        
+                                <Row>
+
+
+
+                                    <Col md={4} >
+                                        <Image fluid src={item.media} className="float-sm-start"/>
+                                        {/* <img src={item.media}  className="float-sm-start img-fluid" alt="..." style={{paddingRight:'20px'}}/> */}
+
+                                    </Col>
+                                    <Col md={8}>
+
+                                        {item.summary}
+
+                                    </Col>
+
+                                </Row>
+
+
+                            </Col>
+                         
+                        </Row>
+
                     </ListGroup.Item>
 
                 </div>
@@ -86,17 +119,56 @@ const News = () => {
         return contentsArray;
     }
 
+    const handleClick = () => {
+        loadData(searchtext)
+
+
+    }
+
     return (
         <div>
+
+
+
             <Container>
-                <Row>
-                    <Col>
-                        <ListGroup>
-                            {getContents()}
-                        </ListGroup>
+
+                <Row style={{ marginBottom: '20px', marginTop: '10px' }}>
+                    <Col md={2} >
+                    </Col>
+                    <Col md={8}>
+                        <Row >
+                            <Col >
+                                <Form className="d-flex" >
+                                    <FormControl
+                                        type="text"
+                                        placeholder="Search"
+                                        className="me-2"
+                                        aria-label="Search"
+                                        value={searchtext}
+                                        onChange={(e) => setSearchText(e.target.value)}
+                                    />
+                                    <Button onClick={handleClick} variant="outline-success" type="button" >Search</Button>
+                                </Form>
+                            </Col>
+
+                        </Row>
+                        <Row style={{marginTop:"20px"}}>
+                            <Col>
+                                <ListGroup>
+                                    {getContents()}
+                                </ListGroup>
+
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col md={2}>
                     </Col>
                 </Row>
+
+
+
             </Container>
+
         </div>
     );
 }
