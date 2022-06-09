f55d2eeaff030d6620025e9fe2e5358e6acccc93
Neerej Selvakumar <neerejselvakumar@Neerejs-MacBook-Pro.local>
2022-06-09 19:03:28 -0400

    fix guardian changes

--- src/Guardian.js
+++ src/Guardian.js
@@ -1,15 +1,15 @@
-import { useState , useEffect } from "react";
-import { Container, Row , Col, ListGroupItem, Form, FormControl, Button} from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Container, Row, Col, ListGroupItem, Form, FormControl, Button } from "react-bootstrap";
 import Header from './Header.js';
-import axios from 'axios';
+
 
 const Guardian = () => {
-    
-    const [news, setNews] = useState([]);
-    const [searchtext, setSearchText] = useState([])
 
-    const API_KEY = '08a46ee6-3582-46b5-b4ef-87a7578e48f1';
-    //const API_URL = 'https://content.guardianapis.com/search?api-key=08a46ee6-3582-46b5-b4ef-87a7578e48f1';
+    const [news, setNews] = useState([]);
+    const [searchtext, setSearchText] = useState("")
+
+    //const API_KEY = '08a46ee6-3582-46b5-b4ef-87a7578e48f1';
+    const API_URL = 'https://content.guardianapis.com/search?api-key=08a46ee6-3582-46b5-b4ef-87a7578e48f1';
 
     useEffect(() => {
         loadData();
@@ -21,50 +21,62 @@ const Guardian = () => {
     //     const data = await response.json();
     //     setNews(data.response.results);
     //     console.log(news);
-        
+
     // }
 
 
     const loadData = async (searchvalue) => {
-       
-        var options;
+
+        const response = await fetch(API_URL + "&q=" + searchvalue);
+        const data = await response.json();
+        setNews(data.response.results);
+        console.log(news);
+
+        
 
         if (searchvalue) {
 
-            options = {
-                method: 'GET',
-                url: 'https://content.guardianapis.com/search',
-                params: {
-                    q: searchvalue,
-                    lang: 'en',
-                    sort_by: 'relevancy',
-                    page: '1'
-                },
-                headers: {
-                    'x-api-key': API_KEY
-                }
-            };
+            // options = {
+            //     method: 'GET',
+            //     url: 'https://content.guardianapis.com/search',
+            //     params: {
+            //         q: searchvalue,
+            //         lang: 'en',
+            //         sort_by: 'relevancy',
+            //         page: '1'
+            //     },
+            //     headers: {
+            //         'x-api-key': API_KEY
+            //     }
+            // };
         }
 
         else {
 
 
-            options = {
-                method: 'GET',
-                url: 'https://content.guardianapis.com/search',
-                params: { q: 'Bitcoin', lang: 'en', sort_by: 'relevancy', page: '1' },
-                headers: {
-                    'x-api-key': API_KEY
-                }
-            };
+            // options = {
+            //     method: 'GET',
+            //     url: 'https://content.guardianapis.com/search',
+            //     params: { q: 'Russia', lang: 'en', sort_by: 'relevancy', page: '1' },
+            //     headers: {
+            //         'x-api-key': API_KEY
+            //     }
+            //};
+
+            const response = await fetch(API_URL);
+            const data = await response.json();
+            setNews(data.response.results);
+            console.log(news);
         }
 
-        axios.request(options).then(function (res) {
-            console.log(res.response);
-            setNews(res.response.results);
-        }).catch(function (error) {
-            //console.error(error);
-        });
+        // axios.request(options).then(function (res) {
+        //     console.log(res.response);
+        //     setNews(res.response.results);
+
+
+        // }).catch(function (error) {
+        //     //console.error(error);
+        // });
 
 
 
@@ -81,8 +93,8 @@ const Guardian = () => {
                             <Col>
                                 <h6>{item.sectionName}</h6>
 
-                               
-                
+
+
                                 <ListGroupItem>
                                     {item.webTitle}
                                 </ListGroupItem>
@@ -93,8 +105,8 @@ const Guardian = () => {
                             </Col>
                         </Row>
                     </Container>
-                    
-                         
+
+
                 </div>
             )
         })
@@ -108,46 +120,46 @@ const Guardian = () => {
 
     }
 
-    return (  
+    return (
 
         <>
             <div>
 
                 <Container>
                     <Row>
-                        <Col style={{marginTop:"10px", marginBottom:"20px"}}>
-                        <Header title="Latest News"/>
-                        <Row style={{ marginTop: "20px" }}>
-                            <Col >
-                                <Form className="d-flex" >
-                                    <FormControl
-                                        type="text"
-                                        placeholder="Search"
-                                        className="me-2"
-                                        aria-label="Search"
-                                        value={searchtext}
-                                        onChange={(e) => setSearchText(e.target.value)}
-                                    />
+                        <Col style={{ marginTop: "10px", marginBottom: "20px" }}>
+                            <Header title="Latest News" />
+                            <Row style={{ marginTop: "20px" }}>
+                                <Col >
+                                    <Form className="d-flex" >
+                                        <FormControl
+                                            type="text"
+                                            placeholder="Search"
+                                            className="me-2"
+                                            aria-label="Search"
+                                            value={searchtext}
+                                            onChange={(e) => setSearchText(e.target.value)}
+                                        />
 
 
-                                    <Button onClick={(e) => handleClick(e)} variant="outline-success" type="submit" >Search</Button>
-                                </Form>
-                            </Col>
+                                        <Button onClick={(e) => handleClick(e)} variant="outline-success" type="submit" >Search</Button>
+                                    </Form>
+                                </Col>
 
-                        </Row>
-                    
+                            </Row>
+
                         </Col>
                     </Row>
                     <Row>
                         <Col>
-                        {getContentsArray()}
+                            {getContentsArray()}
                         </Col>
                     </Row>
                 </Container>
-                
+
             </div>
         </>
     );
 }
- 
+
 export default Guardian;
